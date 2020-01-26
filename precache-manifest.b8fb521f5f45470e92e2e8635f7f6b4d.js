c11564d41e3fcb9a15bde6275e4c8aa3569ba031
CoreAngel <bragon912@gmail.com>
2020-01-26 23:21:11 +0100

    Updates

---
+++ precache-manifest.b8fb521f5f45470e92e2e8635f7f6b4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd475aacb8a088b0147306990218942",
+    "url": "/testly/index.html"
+  },
+  {
+    "revision": "771030677422067d1cbe",
+    "url": "/testly/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "72b1543df855b898414e",
+    "url": "/testly/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "771030677422067d1cbe",
+    "url": "/testly/static/js/2.c524f0d4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/testly/static/js/2.c524f0d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "72b1543df855b898414e",
+    "url": "/testly/static/js/main.8d4e4d81.chunk.js"
+  },
+  {
+    "revision": "fdb82bf38f1ce6b1f2c7",
+    "url": "/testly/static/js/runtime-main.d7030ce2.js"
+  }
+]);
