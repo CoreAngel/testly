c2a3c293bd0ae2350947b2b5251c9b62de51ee20
CoreAngel <bragon912@gmail.com>
2020-01-28 12:45:28 +0100

    Search bar

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from "styled-components";
+import {Input} from "reactstrap";
 import Question from './Question'
 
 const QuestionsList = ({questions}) => {
+    const [searchState, setSearchState] = useState('');
+
+    const searchedQuestions = questions.filter(item => {
+        const question = item.q.toLowerCase();
+        const searchText = searchState.trim().toLowerCase();
+        return question.includes(searchText)
+    });
 
     return (
-        <div>
-            {questions.map((item, index) =><Question key={index} number={index + 1} question={item.q} answers={item.a} correct={item.c}/>)}
-        </div>
+        <>
+            <SearchContainer>
+                <Input onChange={({target}) => setSearchState(target.value)} value={searchState} type='search'/>
+            </SearchContainer>
+            <div>
+                {searchedQuestions.map((item, index) =><Question key={index} number={index + 1} question={item.q} answers={item.a} correct={item.c}/>)}
+            </div>
+        </>
     )
 };
+
+const SearchContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+`;
 
 export default QuestionsList;
