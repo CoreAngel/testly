15a846411dc321792590b39e7eddb3551050ef98
CoreAngel <bragon912@gmail.com>
2020-02-24 15:18:48 +0100

    redux persist

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from 'redux/store';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import GlobalStyleWrapper from 'theme/GlobalStyleWrapper';
 import MainView from 'views/MainView';
+import Spinner from 'components/Spinner';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
     return (
         <Provider store={store}>
-            <GlobalStyleWrapper>
-                <MainView />
-            </GlobalStyleWrapper>
+            <PersistGate loading={Spinner} persistor={persistor}>
+                <GlobalStyleWrapper>
+                    <MainView />
+                </GlobalStyleWrapper>
+            </PersistGate>
         </Provider>
     );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { persistStore, persistReducer, FLUSH, REGISTER, REHYDRATE, PAUSE, PURGE, PERSIST } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import listReducer from './listReducer';
 import testReducer from './testReducer';
 import optionsReducer from './optionsReducer';
 
-const store = configureStore({
-    reducer: {
-        list: listReducer,
-        test: testReducer,
-        options: optionsReducer,
-    },
+const reducer = combineReducers({
+    list: listReducer,
+    test: testReducer,
+    options: optionsReducer,
 });
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['options'],
+    blacklist: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+});
+export const persistor = persistStore(store);
